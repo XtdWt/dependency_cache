1158eefbe7802975b89de3c04c728c0041040350
houru <houruxuan01@gmail.com>
2026-07-23 19:45:57 +1000

    (feat) refactor base class out of lib and into own file

--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,11 @@
 mod dependency_graph;
+mod dependency_cache_base;
 
-use pyo3::exceptions::{PyTypeError, PyValueError};
 use pyo3::prelude::*;
 use pyo3::types::{PyDict, PyTuple};
+use pyo3::exceptions::{PyTypeError, PyValueError};
 
-use std::collections::HashMap;
-
-use dependency_graph::MethodDependencyGraph;
-
-#[pyclass(subclass)]
-pub struct DependencyCacheBase {
-    cache: HashMap<String, Py<PyAny>>,
-    method_dependency_graph: MethodDependencyGraph,
-}
-
-#[pymethods]
-impl DependencyCacheBase {
-    #[new]
-    #[pyo3(signature = (*_args, **_kwargs))]
-    fn new(_args: &Bound<'_, PyTuple>, _kwargs: Option<&Bound<'_, PyDict>>) -> Self {
-        Self {
-            cache: HashMap::new(),
-            method_dependency_graph: MethodDependencyGraph::new(),
-        }
-    }
-
-    fn get_cached_value(&self, py: Python<'_>, name: &str) -> Option<Py<PyAny>> {
-        if self.method_dependency_graph.is_valid(name.to_string()) {
-            return self.cache.get(name).map(|obj| obj.clone_ref(py));
-        }
-        None
-    }
-
-    fn set_cached_value(&mut self, name: &str, value: Py<PyAny>) {
-        self.cache.insert(name.to_string(), value);
-        self.method_dependency_graph.validate(name.to_string());
-    }
-
-    fn update_cached_value(&mut self, name: &str, value: Py<PyAny>) {
-        self.method_dependency_graph.invalidate(name.to_string());
-        self.cache.insert(name.to_string(), value);
-        self.method_dependency_graph.validate(name.to_string());
-    }
-
-    fn clear_cache(&mut self) {
-        self.cache.clear();
-    }
-
-    fn current_cache<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyDict>> {
-        let dict = PyDict::new(py);
-        for (name, value) in &self.cache {
-            dict.set_item(name, value.clone_ref(py))?;
-        }
-        Ok(dict)
-    }
-
-    fn current_graph<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyDict>> {
-        let dict = PyDict::new(py);
-        for (name, value) in &self.method_dependency_graph.cache_dependency_graph {
-            dict.set_item(name, value)?;
-        }
-        Ok(dict)
-    }
-
-    fn current_cache_validation<'py>(&self, py: Python<'py>) -> PyResult<Bound<'py, PyDict>> {
-        let dict = PyDict::new(py);
-        for (name, value) in &self.method_dependency_graph.cache_validation {
-            dict.set_item(name, value)?;
-        }
-        Ok(dict)
-    }
-}
-
+use dependency_cache_base::DependencyCacheBase;
 
 #[pyclass(name = "dependency_cache", frozen)]
 pub struct DependencyCacheDecoratorFactory {
