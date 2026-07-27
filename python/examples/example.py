57f39c522fe309fb1cc7e7c48b532f821186846d
Tony <111484659+XtdWt@users.noreply.github.com>
2026-07-27 19:43:06 +1000

    (feat) add readme, add license (#4)
    * (feat) add empty readme, add license
    
    * (feat) add a simple example to example.py and rename existing example
    file
    
    * (feat) add readme
    
    * (lint) add newline to readme

--- python/examples/example.py
+++ python/examples/example.py
@@ -1,10 +1,8 @@
-from dependency_cache import DependencyCacheBase, dependency_cached, plot_dependency_graph
+from dependency_cache import DependencyCacheBase, automagically_dependency_cached
 
 
-class ExampleCalculationManual(DependencyCacheBase):
-    """D     E
-        \\  //
-          C
+class ExampleCalculation(DependencyCacheBase):
+    """   C
         //  \\
        A      B
     """
@@ -14,43 +12,29 @@ class ExampleCalculationManual(DependencyCacheBase):
         self.x = x
         self.y = y
 
-    @dependency_cached()
+    @automagically_dependency_cached()
     def A(self):
         print("calculating A")
         return self.x
 
-    @dependency_cached()
+    @automagically_dependency_cached()
     def B(self):
         print("calculating B")
         return self.y
 
-    @dependency_cached(dependencies=["A", "B"])
+    @automagically_dependency_cached()
     def C(self):
         print("calculating C")
-        return self.A() + self.B()
-
-    @dependency_cached(dependencies=["C"])
-    def D(self):
-        print("calculating D")
-        return self.C() / 2
-
-    @dependency_cached(dependencies=["C"])
-    def E(self):
-        print("calculating E")
-        return self.C() * 2
+        return self.B() + self.A()
 
 
 if __name__ == "__main__":
-    print("D     E")
-    print(" \\   /")
     print("   C")
     print(" /   \\")
     print("A     B")
-    c = ExampleCalculationManual(6, 7)
-    print(f"Result of E = {c.E()}")  # calculates all, prints 26
-    c.update_cached_value("A", 0)  # invalidates C, E
-    print(c.current_cache(), c.current_graph(), c.current_cache_validation())
-    print(f"Result of E = {c.E()}")  # recalculates C, E, returns 14
-    print(f"Result of D = {c.D()}")  # recalculates D, returns 3.5
-
-    # plot_dependency_graph(c, with_labels=True)
+    c = ExampleCalculation(3, 5)
+    print(f"cache={c.current_cache()} graph={c.current_graph()} validation={c.current_cache_validation()}")
+    print(f"Result of C = {c.C()}")  # calculates all, prints 8
+    print(f"Result of C = {c.C()}")  # hits cache, prints 8
+    c.update_cached_value("A", 0)  # invalidates C
+    print(f"Result of E = {c.C()}")  # recalculates C returns 5
