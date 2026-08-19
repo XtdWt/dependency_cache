47509ab76a9648adb4b0527247fbcb03d8d5461b
houru <houruxuan01@gmail.com>
2026-08-19 14:31:18 +1000

    (feat) make the function calls example slightly more complicated

--- python/examples/function_calls_example.py
+++ python/examples/function_calls_example.py
@@ -1,4 +1,4 @@
-from dependency_cache import DependencyCacheBase, dependency_cached
+from dependency_cache import DependencyCacheBase, dependency_cached, plot_dependency_graph
 
 
 class FunctionCallObj(DependencyCacheBase):
@@ -6,10 +6,10 @@ class FunctionCallObj(DependencyCacheBase):
         super().__init__()
         self.x = x
 
-    @dependency_cached()
+    @dependency_cached(dependencies=[("C", {})])
     def A(self, x):
         print("calculating A!")
-        return x + self.x
+        return x + self.x + self.C()
 
     @dependency_cached(dependencies=[("A", {"x": 0}), ("A", {"x": 1})])
     def B(self):
@@ -19,12 +19,17 @@ class FunctionCallObj(DependencyCacheBase):
             total += self.A(i)
         return total
 
+    @dependency_cached()
+    def C(self):
+        print("calculating C!")
+        return 3
+
 
 if __name__ == "__main__":
     print("dependency graph can have each function call cached")
     c = FunctionCallObj(1)
 
-    print(f"Result of B {c.B()}")  # calculates parent B and does calculates A(0), A(1)
+    print(f"Result of B {c.B()}")  # calculates parent B and does calculates A(0), C, A(1)
 
     print(f"Result of B {c.B()}")  # cache works for B, no more prints
     print(f"Result of A {c.A(0)}")
