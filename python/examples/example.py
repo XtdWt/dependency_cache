04d3ce67dcba3f865b706f59aa7613312e2a1163
houru <houruxuan01@gmail.com>
2026-09-10 17:17:28 +1000

    (feat) remove, fails correctly

--- python/examples/example.py
+++ python/examples/example.py
@@ -28,7 +28,6 @@ class ExampleCalculation(DependencyCacheBase):
         return self.B() + self.A()
 
 
-withkasdfjkasdjflasjkfaslkjdflaksjdf
 if __name__ == "__main__":
     print("example of using dependency_cache for recomputation")
     print("   C")
